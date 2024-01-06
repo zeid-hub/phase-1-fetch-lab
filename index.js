35cf664c9b196ae50252570fa3884814a299727d
zeid <zeidhassan4@gmail.com>
2024-01-06 17:39:30 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    renderBooks(json);
+  });
 }
 
 function renderBooks(books) {
